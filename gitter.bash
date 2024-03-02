e4d0521bb535f20e766515b7cc703856f67280ae
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-03-02 16:21:47 -0700

    Update gitter.bash

--- gitter.bash
+++ gitter.bash
@@ -1,1 +1,13 @@
 # Put gitter here
+git clone -b stable https://github.com/HttpAnimation/NAME.git
+mv NAME stable
+git clone -b blank https://github.com/HttpAnimation/NAME.git
+mv NAME blank 
+git clone -b extra https://github.com/HttpAnimation/NAME.git
+mv NAME extra
+git clone -b gh-pages https://github.com/HttpAnimation/NAME.git
+mv NAME gh-pages
+git clone -b gitter https://github.com/HttpAnimation/NAME.git
+mv NAME gitter
+git clone -b main https://github.com/HttpAnimation/NAME.git
+mv NAME main
